f124a6ce3219e6281312ad1768498b6b39957b3d
ajiiz <opticpiter@gmail.com>
2020-10-11 15:06:18 +0200

    Changed the structure

--- src/main/java/com/mycompany/PioGame/PioGame.java
+++ src/main/java/com/mycompany/PioGame/PioGame.java
@@ -1,8 +1,8 @@
-package com.mycompany.lab_1;
+package com.mycompany.PioGame;
 
 import java.util.Random;
 
-public class lab_1 {
+public class PioGame {
     public static void main(String[] args) {
         Random r = new Random();
         int rValue;
@@ -10,7 +10,9 @@ public class lab_1 {
         do {
             rValue = r.nextInt(6)+1;
             nValue = r.nextInt(6)+1;
+            System.out.println("Kostka: "+ rValue);
+            System.out.println("Gracz: "+ nValue);
         } while(rValue != nValue);
-        System.out.println("Wylosowana: " + rValue + " ,wytypowana: " + nValue);
+        System.out.println("Wylosowana: " + rValue + ". Wytypowana: " + nValue + ". Brawo!");
     }
 }
